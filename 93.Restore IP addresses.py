905fcffa88388252c318a753a4c21f3435552c4f
Eliyas <147948700+Eliyas47@users.noreply.github.com>
2026-01-25 00:06:33 +0300

    Create 93.Restore IP addresses.py

---
+++ 93.Restore IP addresses.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        res = []
+        
+        def backtrack(start: int, path: List[str]):
+            # If we have 4 segments and used all characters
+            if len(path) == 4:
+                if start == len(s):
+                    res.append(".".join(path))
+                return
+            
+            # Try segments of length 1 to 3
+            for length in range(1, 4):
+                if start + length > len(s):
+                    break
+                segment = s[start:start+length]
+                
+                # Check validity
+                if (segment.startswith("0") and len(segment) > 1) or int(segment) > 255:
+                    continue
+                
+                backtrack(start + length, path + [segment])
+        
+        backtrack(0, [])
+        return res
