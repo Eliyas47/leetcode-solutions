7d55010682ab18e2280dd089732cf5b7678af64a
Eliyas <147948700+Eliyas47@users.noreply.github.com>
2026-02-13 22:44:13 +0300

    Create 283. Move Zeroes.py

---
+++ 283. Move Zeroes.py
@@ -0,0 +1,14 @@
+class Solution:
+    def moveZeroes(self, nums):
+        # Pointer for the position of the next non-zero element
+        last_non_zero = 0
+
+        # Move non-zero elements forward
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[last_non_zero] = nums[i]
+                last_non_zero += 1
+
+        # Fill the remaining positions with zeros
+        for i in range(last_non_zero, len(nums)):
+            nums[i] = 0
