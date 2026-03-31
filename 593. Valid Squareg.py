0eb4ea6ae149c36b60e6da85bc49488aab3bda77
Eliyas Siyum Biratu <147948700+Eliyas47@users.noreply.github.com>
2026-03-31 10:51:27 +0300

    Create 593. Valid Squareg.py

---
+++ 593. Valid Squareg.py
@@ -0,0 +1,16 @@
+class Solution:
+    def validSquare(self, p1, p2, p3, p4):
+        def dist(a, b):
+            return (a[0]-b[0])**2 + (a[1]-b[1])**2
+        
+        points = [p1, p2, p3, p4]
+        dists = []
+        
+        for i in range(4):
+            for j in range(i+1, 4):
+                dists.append(dist(points[i], points[j]))
+        
+        dists.sort()
+        
+        # 4 equal sides, 2 equal diagonals
+        return dists[0] > 0 and dists[0] == dists[1] == dists[2] == dists[3] and dists[4] == dists[5]
