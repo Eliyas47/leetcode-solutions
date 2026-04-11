1aecf8300154e7b2d917729f2cb9cfa5e446e6d1
Eliyas Siyum Biratu <147948700+Eliyas47@users.noreply.github.com>
2026-04-11 19:57:51 +0300

    Implement MyCircularQueue with basic operations

---
+++ 622. Design Circular Queue.py
@@ -0,0 +1,35 @@
+class MyCircularQueue:
+
+    def __init__(self, k: int):
+        self.queue = [0] * k
+        self.size = k
+        self.count = 0
+        self.front = 0
+        self.rear = -1
+
+    def enQueue(self, value: int) -> bool:
+        if self.isFull():
+            return False
+        self.rear = (self.rear + 1) % self.size
+        self.queue[self.rear] = value
+        self.count += 1
+        return True
+
+    def deQueue(self) -> bool:
+        if self.isEmpty():
+            return False
+        self.front = (self.front + 1) % self.size
+        self.count -= 1
+        return True
+
+    def Front(self) -> int:
+        return -1 if self.isEmpty() else self.queue[self.front]
+
+    def Rear(self) -> int:
+        return -1 if self.isEmpty() else self.queue[self.rear]
+
+    def isEmpty(self) -> bool:
+        return self.count == 0
+
+    def isFull(self) -> bool:
+        return self.count == self.size
