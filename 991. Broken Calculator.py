7c3feac5aef3e96d1dd5fb9b5a70d0abf8ea20de
Eliyas Siyum Biratu <147948700+Eliyas47@users.noreply.github.com>
2026-05-29 14:14:27 +0300

    Create 991. Broken Calculator.py

---
+++ 991. Broken Calculator.py
@@ -0,0 +1,10 @@
+class Solution:
+    def brokenCalc(self, startValue: int, target: int) -> int:
+        operations = 0
+        while target > startValue:
+            operations += 1
+            if target % 2 == 0:
+                target //= 2
+            else:
+                target += 1
+        return operations + (startValue - target)
