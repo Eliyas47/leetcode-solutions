59f7af030d7836afbfca721d85f842c832557dc6
Eliyas Siyum Biratu <147948700+Eliyas47@users.noreply.github.com>
2026-06-03 13:25:34 +0300

    Create 945. Minimum Increment to Make Array Unique.py

---
+++ 945. Minimum Increment to Make Array Unique.py
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minIncrementForUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        long long moves = 0;
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] <= nums[i-1]) {
+                moves += (nums[i-1] + 1 - nums[i]);
+                nums[i] = nums[i-1] + 1;
+            }
+        }
+        return (int)moves;
+    }
+};
